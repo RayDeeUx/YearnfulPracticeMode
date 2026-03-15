90ccd2ad8337947fcd31c0189db9753d9a5d29a6
Erymanthus (RayDeeUx) <raydeeux@gmail.com>
2026-03-15 19:53:00 -0400

    some housekeeping

--- src/PlayerObject.cpp
+++ src/PlayerObject.cpp
@@ -16,12 +16,12 @@ class $modify(MyPlayerObject, PlayerObject) {
 	bool isFlyingAndYearning() {
 		return m_isShip || m_isBird || m_isDart || m_isSwing;
 	}
-	void resetTimer() {
+	void resetYearnfulCheckpointTimer() {
 		m_fields->yearningLastCheckpointTime = 0.0;
 	}
 	void hitGround(GameObject* object, bool unknown) {
 		PlayerObject::hitGround(object, unknown);
-		if (!m_gameLayer || m_gameLayer->m_isEditor || !isEnabled || m_isDead) return MyPlayerObject::resetTimer();
+		if (!m_gameLayer || m_gameLayer->m_isEditor || !isEnabled || m_isDead) return MyPlayerObject::resetYearnfulCheckpointTimer();
 		PlayLayer* playLayer = static_cast<PlayLayer*>(m_gameLayer);
 		if (playLayer->m_isPracticeMode || playLayer->m_isTestMode || !m_fields->canCheckpointNow || this != playLayer->m_player1 || this->isFlyingAndYearning() || m_isSpider) return;
 		CheckpointObject* checkpoint = playLayer->markCheckpoint();
@@ -30,14 +30,14 @@ class $modify(MyPlayerObject, PlayerObject) {
 	}
 	void incrementJumps() {
 		PlayerObject::incrementJumps();
-		if (!m_gameLayer || m_gameLayer->m_isEditor || !isEnabled || m_isDead) return MyPlayerObject::resetTimer();
+		if (!m_gameLayer || m_gameLayer->m_isEditor || !isEnabled || m_isDead) return MyPlayerObject::resetYearnfulCheckpointTimer();
 		PlayLayer* playLayer = static_cast<PlayLayer*>(m_gameLayer);
 		if (playLayer->m_isPracticeMode || playLayer->m_isTestMode || m_fields->canCheckpointNow || this != playLayer->m_player1 || this->isFlyingAndYearning() || m_isSpider) return;
 		m_fields->canCheckpointNow = true;
 	}
 	void playerDestroyed(bool otherPlayer) {
 		PlayerObject::playerDestroyed(otherPlayer);
-		if (!m_gameLayer || m_gameLayer->m_isEditor || !isEnabled) return MyPlayerObject::resetTimer();
+		if (!m_gameLayer || m_gameLayer->m_isEditor || !isEnabled) return MyPlayerObject::resetYearnfulCheckpointTimer();
 		PlayLayer* playLayer = static_cast<PlayLayer*>(m_gameLayer);
 		if (playLayer->m_isPracticeMode || playLayer->m_isTestMode || (this != playLayer->m_player1 && this != playLayer->m_player2)) return;
 		if (!autoPracticeMode && playLayer->m_checkpointArray && playLayer->m_checkpointArray->count() > 0) {
@@ -49,7 +49,7 @@ class $modify(MyPlayerObject, PlayerObject) {
 	}
 	void update(float dt) {
 		PlayerObject::update(dt);
-		if (!m_gameLayer || m_gameLayer->m_isEditor || !isEnabled || m_isDead) return MyPlayerObject::resetTimer();
+		if (!m_gameLayer || m_gameLayer->m_isEditor || !isEnabled || m_isDead) return MyPlayerObject::resetYearnfulCheckpointTimer();
 		PlayLayer* playLayer = static_cast<PlayLayer*>(m_gameLayer);
 		if (playLayer->m_isPracticeMode || playLayer->m_isTestMode || (this != playLayer->m_player1 && this != playLayer->m_player2) || (!this->isFlyingAndYearning() && !m_isSpider)) return;
 		m_fields->yearningLastCheckpointTime += dt;
